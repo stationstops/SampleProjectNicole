799d85fbe1ff58dbfb2e8b6bf8301e57b72fa07e
Chris Schoenfeld <chris.schoenfeld@pointpickup.com>
2021-07-09 11:37:30 -0400

    Initial Commit

---
+++ SampleProjectNicole/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleProjectNicole
+//
+//  Created by Chris Schoenfeld on 7/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
